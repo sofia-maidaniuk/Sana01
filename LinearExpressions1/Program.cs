f7dbcedef00300a1221d6eba6e57aca1438e6dec
sofia_maidaniuk <ipz231_msye@student.ztu.edu.ua>
2023-11-07 14:45:11 +0200

    sana01

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System; 
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+        System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+        customCulture.NumberFormat.NumberDecimalSeparator = ".";
+        System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+        Console.Write("Input double a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Input double b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Input double c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        Console.Write("Input double d: ");
+        double d = double.Parse(Console.ReadLine());
+
+        double x;
+        double y;
+        double z;
+        double r;
+
+        x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (a * a) / (b * b);
+        y = (5 * (a + b) * (c - d)) / (1 / 2.0 * c) + d * d * ((a * a - b * b) / (b - a));
+        z = (Math.Pow((x * x - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
+        r = (1.0 / 2 * a + 3.0 / 4 * b - 7.0 / 5) / (3 * c + 1) + 1 / (a - c);
+        
+        Console.WriteLine("\nResults: ");
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("y = " + y);
+        Console.WriteLine("z = " + z);
+        Console.WriteLine("r = " + r);
+    }
+}
+
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+        System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+        customCulture.NumberFormat.NumberDecimalSeparator = ".";
+        System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+        Console.Write("Input double n: ");
+        double m = double.Parse(Console.ReadLine());
+
+        Console.Write("Input double m: ");
+        double n = double.Parse(Console.ReadLine());
+
+        Console.Write("Input double a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Input double b: ");
+        double b = double.Parse(Console.ReadLine()); 
+        
+        Console.Write("Input double x: ");
+        double x = double.Parse(Console.ReadLine());
+        
+        double y;
+        double z1;
+        double z2;
+
+        z1 = ((m-1)*Math.Sqrt(m) - (n-1) * Math.Sqrt(n))/ (Math.Sqrt(Math.Pow(m,3) * n) + n*m + Math.Pow(m,2) - m);
+        z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+        y = 2.4 * Math.Abs((Math.Pow(m,2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2)*(x - b);
+
+        Console.WriteLine("\nResults: ");
+        Console.WriteLine("y = " + y);
+        Console.WriteLine("z1 = " + z1);
+        Console.WriteLine("z2 = " + z2);
+    }
+}
+
